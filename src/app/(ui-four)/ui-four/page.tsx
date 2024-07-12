d62bf02889e83606e094887e264a250df1483b35
bhanudova02 <bhanudova02@gmail.com>
2024-07-12 12:33:39 +0530

    new ui changes added

--- src/app/(ui-four)/ui-four/page.tsx
+++ src/app/(ui-four)/ui-four/page.tsx
@@ -1,4 +1,3 @@
-import CategoryList from '@/app/components/UI_Four/CategoryList'
 import UI_Four_Footer from '@/app/components/UI_Four/UI_Four_Footer'
 import UI_Four_Hero from '@/app/components/UI_Four/UI_Four_Hero'
 import React from 'react'
@@ -7,7 +6,7 @@ const page = () => {
   return (
     <div>
       <UI_Four_Hero />
-      <CategoryList />
+      
       <UI_Four_Footer />
     </div>
   )
